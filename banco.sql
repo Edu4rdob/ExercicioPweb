c586ddf89554bd09170264b30e47a7ca6484d87f
Edu4rdob <86166126+Edu4rdob@users.noreply.github.com>
2021-07-14 11:16:12 -0300

    Add files via upload

---
+++ banco.sql
@@ -0,0 +1,9 @@
+create database banco;
+use banco;
+
+create table usuarios(	
+nome varchar(100), usuario varchar(50), email varchar(50), senha varchar(50),
+constraint usuarioPK primary key(usuario),
+constraint userU unique (usuario),
+constraint emailU unique (email)
+);
